71c2a06759fdf3f3bf2913653835721239f20fd8
binhdao29 <binhchan0293@gmail.com>
2021-03-06 11:08:36 +0700

    using classnames

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,16 +1,14 @@
 import React, { Component } from 'react';
 import './TodoItem.css';
-
+import classNames from 'classnames';
 
 class TodoItem extends Component {
     render() {
         const { item } = this.props;
-        let className = 'TodoItem';
-        if (item.isComplete) {
-            className += ' TodoItem-complete';
-        }
         return (
-            <div className={className}>
+            <div className={classNames('TodoItem', {
+                'TodoItem-complete': item.isComplete
+            })}>
                 <p>{ item.title }</p>
             </div>
         );
